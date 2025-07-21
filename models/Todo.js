1c485ab018177d10d8aaf7f7eb417111d277f465
unknown <waghvinod4625@gmail.com>
2025-07-21 09:51:15 +0530

    Fix Todo Schema

--- models/Todo.js
+++ models/Todo.js
@@ -1,29 +1,25 @@
 const mongoose = require("mongoose");
-const { date } = require("zod");
-const { required, maxLength } = require("zod/mini");
-const { Schema } = require("zod/v3");
 
 const todoSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
-    maxLength: 50,
+    maxlength: 50,
   },
   description: {
     type: String,
     required: true,
-    maxLength: 50,
+    maxlength: 50,
   },
   createdAt: {
     type: Date,
-    required: true,
-    default: Date.now(),
+    default: Date.now,
   },
   updatedAt: {
     type: Date,
-    required: true,
-    default: Date.now(),
+    default: Date.now,
   },
 });
 
 module.exports = mongoose.model("Todo", todoSchema);
+
